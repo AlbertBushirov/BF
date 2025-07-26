e15dfd733f7f06958d13ae122763f95de87d775b
Albert Bashirov <albash7@yandex.ru>
2025-07-26 18:53:51 +0300

    feat: функционал вкладки турнир

--- src/components/view/Tournament.ts
+++ src/components/view/Tournament.ts
@@ -13,6 +13,8 @@ interface ITournamentView {
 }
 
 export class Tournament extends Component<ITournamentView> {
+	protected tournamentName: HTMLElement;
+	protected tournamentListOne: HTMLElement;
 	public _day: HTMLElement;
 	public _day_two: HTMLElement;
 	public _games: HTMLElement;
@@ -21,7 +23,31 @@ export class Tournament extends Component<ITournamentView> {
 		super(container, new EventEmitter());
 		this._day = ensureElement<HTMLElement>('#day_one', container);
 		this._day_two = ensureElement<HTMLElement>('#day_two', container);
-
+		this.tournamentName = container.querySelector('.tournament__name');
+		this.tournamentListOne = container.querySelector('.tournament__list_one');
+		console.log(this.tournamentName);
 		this.events = events;
+
+		localTournament.forEach((element) => {
+			const result = this.tournamentRender(element);
+		});
+	}
+
+	tournamentRender(tournament: ITournament) {
+		const tournamentTemplate = document.querySelector(
+			'#tournament-item'
+		) as HTMLTemplateElement;
+		const tournamentElement = tournamentTemplate.content;
+
+		const container = tournamentElement.querySelector('.tournament__list');
+		const cloneContainer = container.cloneNode(true) as HTMLElement;
+
+		cloneContainer.querySelector('.tournament__name').textContent =
+			tournament.title;
+
+		const allNames = tournament.participant.map((el) => el.name.name).join();
+		cloneContainer.querySelector('.tournament__player').textContent = allNames;
+
+		return cloneContainer;
 	}
 }
